1ed00955c043cabffaeb527cb937f2e577c2d396
Tyler Kirkwood <tkirkwood@myseneca.ca>
2023-06-07 07:22:11 -0400

    adding practice folder

---
+++ ops_practice/data_structures.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python3
+# Tyler Kirkwood
+
+t1 = ('Prime', 'Ix', 'Secundus', 'Caladan')
+t2 = (1, 2, 3, 4, 5, 6)
+
+for item in t1:
+    print('item: ' + item)
---
+++ ops_practice/function.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+# Tyler Kirkwood 
+
+
+def return_text_value():
+    name = 'terry'
+    greeting = 'good morning ' + name
+    return greeting
+text = return_text_value()
+
+
+print(text)
